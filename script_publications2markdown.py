64bfbbd12ebfb20020e836bb5f0c012347e7e4fb
Baptiste Caramiaux <baptiste.caramiaux@gmail.com>
2026-07-01 16:18:51 +0200

    updates

--- script_publications2markdown.py
+++ script_publications2markdown.py
@@ -6,18 +6,30 @@ sorted_df = df.sort_values(by=["YEAR", "AUTHORS"], ascending=[False, True])
 
 print(sorted_df.head())
 
-markdown_lines = []
+markdown_lines = {}
 for _, row in sorted_df.iterrows():
     authors = row["AUTHORS"]
     title = row["PUPLICATION TITLE"]
     year = row["YEAR"]
     conference = row["CONFERENCE / JOURNAL"]
     url = row["URL"]
+    if year not in markdown_lines.keys():
+        markdown_lines[year] = {}
+    if conference not in markdown_lines[year].keys():
+        markdown_lines[year][conference] = []
     markdown_line = f"- {authors} ({year}). **{title}**. *{conference}*. [{url}]({url})"
-    markdown_lines.append(markdown_line)
+    markdown_lines[year][conference].append(markdown_line)
 
-markdown_output = "\n".join(markdown_lines)
+# markdown_output = "\n".join(markdown_lines)
 
 # Save to a markdown file
 with open("publications.md", "w", encoding="utf-8") as f:
-    f.write(markdown_output)
+    for y in sorted(markdown_lines.keys(), reverse=True):
+        f.write(f"# {y}\n")
+        for c in sorted(markdown_lines[y].keys()):
+            f.write(f"# {c}\n")
+            markdown_output = "\n".join(markdown_lines[y][c])
+            f.write(markdown_output)
+            print(y, c, len(markdown_lines[y][c]))
+            if len(markdown_lines[y][c]) == 1:
+                f.write("\n")
